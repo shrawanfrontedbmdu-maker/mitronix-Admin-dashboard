6722cab5bc8ee868ced201596ca7d850b82dbdd3
Khush Alam <khush.node.bmdu@gmail.com>
2026-02-17 18:54:25 +0530

    Banner Module

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -66,7 +66,9 @@ import { useState } from "react";
 import { useEffect } from "react";
 import Store from "./pages/stores/Store";
 import AddStore from "./pages/stores/AddStore";
-
+import DelayBanners from "./pages/DelayBanners"
+import DelayBannerCreate from "./pages/DelayBannerCreate";
+import DelayBannerupdate from "./pages/DelayBannerupdate";
 function Admin() {
   const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
@@ -117,153 +119,157 @@ function Admin() {
     <div className="app">
       {isAuthenticated && <Sidebar onLogout={handleLogout} />}
       <div className={`main-content ${isAuthenticated ? '' : 'auth-page'}`}>
-      <Routes>
-        {/* Auth Routes */}
-        <Route index element={isAuthenticated ? <Dashboard /> : <Login role={"admin"}/>} />
+        <Routes>
+          {/* Auth Routes */}
+          <Route index element={isAuthenticated ? <Dashboard /> : <Login role={"admin"} />} />
 
-        {isAuthenticated ? (
-          <Route element={<ProtectedRoute allowedRoles={["admin"]} />}>
-            <Route path="dashboard" element={<Dashboard/>} />
+          {isAuthenticated ? (
+            <Route element={<ProtectedRoute allowedRoles={["admin"]} />}>
+              <Route path="dashboard" element={<Dashboard />} />
 
-            {/* Products Routes */}
-            <Route path="products/list" element={<ProductList />} />
-            <Route path="products/grid" element={<ProductGrid />} />
-            <Route path="products/details/:id" element={<ProductDetails />} />
-            <Route path="products/edit/:id" element={<ProductEdit />} />
-            <Route path="products/edit" element={<ProductEdit />} />
-            <Route path="products/create" element={<CreateProduct />} />
+              {/* Products Routes */}
+              <Route path="products/list" element={<ProductList />} />
+              <Route path="products/grid" element={<ProductGrid />} />
+              <Route path="products/details/:id" element={<ProductDetails />} />
+              <Route path="products/edit/:id" element={<ProductEdit />} />
+              <Route path="products/edit" element={<ProductEdit />} />
+              <Route path="products/create" element={<CreateProduct />} />
 
-            {/*infoCard*/}
-            <Route path="infosection/list" element={<InfoSectionsList />} />
-            <Route
-              path="infosections/create"
-              element={<CreateInfoSection />}
-            />
-            <Route
-              path="infosections/edit/:id"
-              element={<EditInfoSection />}
-            />
-            <Route path="infosections/edit" element={<EditInfoSection />} />
+              {/*infoCard*/}
+              <Route path="infosection/list" element={<InfoSectionsList />} />
+              <Route
+                path="infosections/create"
+                element={<CreateInfoSection />}
+              />
+              <Route
+                path="infosections/edit/:id"
+                element={<EditInfoSection />}
+              />
+              <Route path="infosections/edit" element={<EditInfoSection />} />
 
-            {/* Categories Routes */}
-            <Route path="categories/list" element={<CategoriesList />} />
-            <Route path="categories/create" element={<CreateCategory />} />
-            <Route path="categories/edit/:id" element={<EditCategory />} />
-            <Route path="categories/edit" element={<EditCategory />} />
+              {/* Categories Routes */}
+              <Route path="categories/list" element={<CategoriesList />} />
+              <Route path="categories/create" element={<CreateCategory />} />
+              <Route path="categories/edit/:id" element={<EditCategory />} />
+              <Route path="categories/edit" element={<EditCategory />} />
 
-            {/* Brands Routes */}
-            <Route path="brands/list" element={<BrandsList />} />
-            <Route path="brands/create" element={<CreateBrand />} />
-            <Route path="brands/edit/:id" element={<EditBrand />} />
-            <Route path="brands/edit" element={<EditBrand />} />
+              {/* Brands Routes */}
+              <Route path="brands/list" element={<BrandsList />} />
+              <Route path="brands/create" element={<CreateBrand />} />
+              <Route path="brands/edit/:id" element={<EditBrand />} />
+              <Route path="brands/edit" element={<EditBrand />} />
 
-            {/* Inventory / Stock Control */}
-            <Route path="inventory" element={<InventoryList />} />
+              {/* Inventory / Stock Control */}
+              <Route path="inventory" element={<InventoryList />} />
 
-            {/* Orders Routes */}
-            <Route path="orders/list" element={<OrdersList />} />
-            <Route path="orders/details/:id" element={<OrderDetails />} />
-            <Route path="orders/details" element={<OrderDetails />} />
-            <Route path="orders/add" element={<OrderAdd />} />
-            <Route path="orders/edit/:id" element={<OrderEdit />} />
-            <Route path="orders" element={<OrdersList />} />
+              {/* Orders Routes */}
+              <Route path="orders/list" element={<OrdersList />} />
+              <Route path="orders/details/:id" element={<OrderDetails />} />
+              <Route path="orders/details" element={<OrderDetails />} />
+              <Route path="orders/add" element={<OrderAdd />} />
+              <Route path="orders/edit/:id" element={<OrderEdit />} />
+              <Route path="orders" element={<OrdersList />} />
 
-            {/* Invoices Routes */}
-            <Route path="invoices/list" element={<InvoicesList />} />
-            <Route path="invoices/details/:id" element={<InvoiceDetails />} />
-            <Route path="invoices/details" element={<InvoiceDetails />} />
-            <Route path="invoices/add" element={<InvoiceAdd />} />
-            <Route path="invoices/add/:id" element={<InvoiceAdd />} />
-            <Route path="invoices/edit/:id" element={<InvoiceEdit />} />
-            <Route path="invoices" element={<InvoicesList />} />
+              {/* Invoices Routes */}
+              <Route path="invoices/list" element={<InvoicesList />} />
+              <Route path="invoices/details/:id" element={<InvoiceDetails />} />
+              <Route path="invoices/details" element={<InvoiceDetails />} />
+              <Route path="invoices/add" element={<InvoiceAdd />} />
+              <Route path="invoices/add/:id" element={<InvoiceAdd />} />
+              <Route path="invoices/edit/:id" element={<InvoiceEdit />} />
+              <Route path="invoices" element={<InvoicesList />} />
 
-            {/* Settings & Profile & Notifications */}
-            <Route path="settings" element={<Settings />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="profle/edit" element={<Editprofile />} />
-            <Route path="notifications" element={<Notifications />} />
-            <Route path="service-request" element={<ServiceRequestList />} />
+              {/* Settings & Profile & Notifications */}
+              <Route path="settings" element={<Settings />} />
+              <Route path="profile" element={<Profile />} />
+              <Route path="profle/edit" element={<Editprofile />} />
+              <Route path="notifications" element={<Notifications />} />
+              <Route path="service-request" element={<ServiceRequestList />} />
 
-            {/*Coupons Routes  */}
-            <Route path="coupons/list" element={<CouponsList />} />
-            <Route path="coupons/create" element={<CouponsAdd />} />
-            <Route path="coupons/analystics" element={<CopunsAnalytics />} />
-            <Route path="coupons/update" element={<Couponsedit />} />
-            <Route path="coupons/update/:id" element={<Couponsedit />} />
+              {/*Coupons Routes  */}
+              <Route path="coupons/list" element={<CouponsList />} />
+              <Route path="coupons/create" element={<CouponsAdd />} />
+              <Route path="coupons/analystics" element={<CopunsAnalytics />} />
+              <Route path="coupons/update" element={<Couponsedit />} />
+              <Route path="coupons/update/:id" element={<Couponsedit />} />
 
-            {/*Customer Route */}
-            <Route path="customer/list" element={<CustomerList />} />
-            <Route path="customer/refferal" element={<CustomerRefferal />} />
-            <Route path="customer/profile" element={<UserProfile />} />
-            <Route path="customer/profile/:id" element={<UserProfile />} />
-            {/* Service Requests Routes */}
-            <Route path="service-requests" element={<ServiceRequestList />} />
-            <Route
-              path="service-requests/create"
-              element={<CreateServiceRequest />}
-            />
-            <Route
-              path="service-requests/:id"
-              element={<ServiceRequestDetails />}
-            />
-            <Route
-              path="service-requests/:id/edit"
-              element={<ServiceRequestEdit />}
-            />
+              {/*Customer Route */}
+              <Route path="customer/list" element={<CustomerList />} />
+              <Route path="customer/refferal" element={<CustomerRefferal />} />
+              <Route path="customer/profile" element={<UserProfile />} />
+              <Route path="customer/profile/:id" element={<UserProfile />} />
+              {/* Service Requests Routes */}
+              <Route path="service-requests" element={<ServiceRequestList />} />
+              <Route
+                path="service-requests/create"
+                element={<CreateServiceRequest />}
+              />
+              <Route
+                path="service-requests/:id"
+                element={<ServiceRequestDetails />}
+              />
+              <Route
+                path="service-requests/:id/edit"
+                element={<ServiceRequestEdit />}
+              />
 
-            {/* Blog Routes */}
-            <Route path="blogs" element={<BlogList />} />
-            <Route path="blogs/create" element={<CreateBlog />} />
-            <Route path="blogs/:id" element={<BlogDetails />} />
-            <Route path="blogs/:id/edit" element={<BlogEdit />} />
+              {/* Blog Routes */}
+              <Route path="blogs" element={<BlogList />} />
+              <Route path="blogs/create" element={<CreateBlog />} />
+              <Route path="blogs/:id" element={<BlogDetails />} />
+              <Route path="blogs/:id/edit" element={<BlogEdit />} />
 
-            {/* Banner Routes */}
-            <Route path="banners" element={<BannerList />} />
-            <Route path="banners/create" element={<CreateBanner />} />
-            <Route path="banners/:id" element={<BannerDetails />} />
-            <Route path="banners/:id/edit" element={<BannerEdit />} />
+              {/* Banner Routes */}
+              <Route path="banners" element={<BannerList />} />
+              <Route path="banners/create" element={<CreateBanner />} />
+              <Route path="banners/:id" element={<BannerDetails />} />
+              <Route path="banners/:id/edit" element={<BannerEdit />} />
 
-            {/* Role Routes */}
-            <Route path="roles" element={<RoleList />} />
-            <Route path="roles/create" element={<CreateRole />} />
-            <Route path="roles/:id" element={<RoleDetails />} />
-            <Route path="roles/edit/:id" element={<RoleEdit />} />
+              {/*Delay Banners */}
+              <Route path="delay-banners" element={<DelayBanners />} />
+              <Route path="delay-banners/create" element={<DelayBannerCreate />} />
+              <Route path="delay-banners/update/:id" element={<DelayBannerupdate />} />
+              {/* Role Routes */}
+              <Route path="roles" element={<RoleList />} />
+              <Route path="roles/create" element={<CreateRole />} />
+              <Route path="roles/:id" element={<RoleDetails />} />
+              <Route path="roles/edit/:id" element={<RoleEdit />} />
 
-            {/* Placeholder routes for other nav items */}
+              {/* Placeholder routes for other nav items */}
 
-            <Route
-              path="permissions"
-              element={
-                <div className="page-header">
-                  <h1>Permissions Page</h1>
-                </div>
-              }
-            />
-            <Route
-              path="customers"
-              element={
-                <div className="page-header">
-                  <h1>Customers Page</h1>
-                </div>
-              }
-            />
-            <Route
-              path="stores/list"
-              element={
-               <Store/>
-              }
-            />
-            <Route
-              path="stores/create"
-              element={
-               <AddStore/>
-              }
-            />
-          </Route>
-        ) : (
-          <Route path="*" element={<Login role={"admin"} />} />
-        )}
-      </Routes>
+              <Route
+                path="permissions"
+                element={
+                  <div className="page-header">
+                    <h1>Permissions Page</h1>
+                  </div>
+                }
+              />
+              <Route
+                path="customers"
+                element={
+                  <div className="page-header">
+                    <h1>Customers Page</h1>
+                  </div>
+                }
+              />
+              <Route
+                path="stores/list"
+                element={
+                  <Store />
+                }
+              />
+              <Route
+                path="stores/create"
+                element={
+                  <AddStore />
+                }
+              />
+            </Route>
+          ) : (
+            <Route path="*" element={<Login role={"admin"} />} />
+          )}
+        </Routes>
       </div>
     </div>
   );
